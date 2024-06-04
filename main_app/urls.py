dfe8c49f1753abfe5371b68ebb6282dafb662a4e
xkawixgirlx <jennkosh@gmail.com>
2024-06-04 11:05:05 -0700

    Add Authentication, and begin render of Dynamic Navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('games/', views.games_list, name='games'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')  
+    return render(request, 'about.html')
+
+def games_list(request):
+    return render(request, 'games/games.html')
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('games')
+        else:
+            error_message = "Invalid signup - Please Try Again"
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
