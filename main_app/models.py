ca378ba1da8880d944fc1c0627311bf173298e1d
xkawixgirlx <jennkosh@gmail.com>
2024-06-06 11:00:17 -0700

    Remove notes from Game model, make Note Model

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,6 @@ from datetime import date
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=150)
-    notes = models.TextField(max_length=500)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
@@ -17,4 +16,12 @@ class Game(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'game_id': self.id})
 
-     
+
+class Note(models.Model):
+    content = models.TextField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.content}, {self.updated_at}'    
